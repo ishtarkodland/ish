32a82b9fef1e23f3e90c32127eb32ccd962ee2f1
ishtarkodland <141470253+ishtarkodland@users.noreply.github.com>
2023-08-05 11:00:00 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "ROFL" : "Tanggapan terhadap lelucon",
+            "SHEESH" : "Sedikit ketidaksetujuan",
+            "CREEPY" : "Menakutkan,tidak menyenangkan",
+            "AGGRO" : "Untuk menjadi agresif/marah"
+            }
+word = input("Ketik kata yang tidak Kamu mengerti (GUNAKAN HURUF BESAR SEMUA!): ")
+if word in meme_dict.keys():
+    # Apa yang harus kita lakukan jika kata itu ditemukan?
+    print(meme_dict[word])
+else:
+    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+    print("kata tidak di temukan silahkan coba lagi")
