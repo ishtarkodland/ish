de0730ca2470ca3e9f7cc94c666e2b4a428ce9cb
ishtarkodland <141470253+ishtarkodland@users.noreply.github.com>
2023-08-26 10:51:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
+import discord
 import random
+from discord.ext import commands
+from bot_logic import gen_pass
 
-keyword = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+intents = discord.Intents.default()
+intents.message_content = True
 
-password = ""
+bot = commands.Bot(command_prefix='$', intents=intents)
+guess_game = None
 
-jumblah_password = int(input('mau berapa banyak password yang dihasilkan?'))
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
 
-for i in range(jumblah_password)
-    password += random.choice(keyword)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
 
-print(keyword+random.choice(keyword))
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def pwd(ctx):
+    await ctx.send(gen_pass())
+
+@bot.command()
+async def guess(ctx):
+    global guess_game
+
+    guess_game = random.randint(1, 10)
+    
+    await ctx.send("Tebak angka 1 sampai 10, jawab dengan $answer")
+
+@bot.command()
+async def answer(ctx, number):
+    if guess_game == int(number):
+        await ctx.send("Selamat anda benar")
+    else:
+        await ctx.send("Jawaban salah, silahkan coba lagi")
+
+
+bot.run("TOKEN RAHASIA ADA DI SINI")
